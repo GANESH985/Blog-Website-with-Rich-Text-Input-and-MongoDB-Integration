92a6593b673501a0d868a193482a7649caa65cbf
GANESH NAIK MUDAVATH <86554572+GANESH985@users.noreply.github.com>
2025-07-01 14:05:50 +0530

    Update postService.js

--- src/services/postService.js
+++ src/services/postService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
   ? '/api' 
-  : 'http://localhost:5000/api';
+  : 'https://blog-website-with-rich-text-input-and.onrender.com/api';
 
 export class PostService {
   static async createPost(data) {
